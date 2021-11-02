9bd263c02620d8f82aed21aede07ebebd94eb48c
Kot-el-kov <79655550+Kot-el-kov@users.noreply.github.com>
2021-11-02 21:57:15 +0300

    d
    d

--- main/src/main/java/com/github/kotelkov/pms/dao/DatabaseInterface.java
+++
@@ -1,5 +0,0 @@
-package com.github.kotelkov.pms.dao;
-
-public interface DatabaseInterface {
-    public String doTask();
-}
--- main/src/main/java/com/github/kotelkov/pms/service/ServiceInterface.java
+++
@@ -1,5 +0,0 @@
-package com.github.kotelkov.pms.service;
-
-public interface ServiceInterface {
-    public String doTask();
-}
