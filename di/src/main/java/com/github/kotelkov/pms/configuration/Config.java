1d790b03c906c5d17cb0766e72f49905fc0e59fe
Kot-el-kov <79655550+Kot-el-kov@users.noreply.github.com>
2021-11-02 20:07:54 +0300

    [homework-3-ioc] di,main
    [homework-3-ioc] di,main

---
+++ di/src/main/java/com/github/kotelkov/pms/configuration/Config.java
@@ -0,0 +1,29 @@
+package com.github.kotelkov.pms.configuration;
+
+import com.github.kotelkov.pms.inject.annotation.Value;
+
+import java.lang.reflect.Field;
+
+public class Config {
+    private final Field field;
+    private final String configName;
+    private final String propertyName;
+
+    public Config(Field field) {
+        this.field = field;
+        this.configName = field.getAnnotation(Value.class).configName().toLowerCase();
+        this.propertyName = field.getAnnotation(Value.class).propertyName().toLowerCase();
+    }
+
+    public Field getField() {
+        return field;
+    }
+
+    public String getConfigName() {
+        return configName;
+    }
+
+    public String getPropertyName() {
+        return propertyName;
+    }
+}
